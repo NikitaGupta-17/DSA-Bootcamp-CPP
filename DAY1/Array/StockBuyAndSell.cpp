e13eb621db810946be4e8711c68faf7d5ba1e276
NikitaGupta-17 <75840623+NikitaGupta-17@users.noreply.github.com>
2021-08-16 21:12:36 +0530

    Add files via upload

--- DAY1/Array/StockBuyAndSell.cpp
+++ DAY1/Array/StockBuyAndSell.cpp
@@ -12,18 +12,23 @@ int main()
     {
         cin >> arr[i];
     }
-    int buy ,sell,j;
-    for(i=0;i<size;i++)
+    int buy ,sell,j,temp=0;
+    for(i=0;i<size;)
     {
         buy=arr[i];
-        for(j=i+2;j<size;j++)
+        for(j=i+1;j<size;j++)
         {
-            if(arr[i]>arr[j])
+            if(arr[j]>arr[i] && arr[j]>arr[j+1])
             {
-                sell = arr[j-1];
-                cout<<"Buy at "<<i+1<<"and sell at "<<j;
+                sell = arr[j];
+                cout<<"Buy at "<<i<<" and sell at "<<j<<endl;
+                i=j+1;
+                temp++;
+                break;
             }
         }
     }
+    if (temp == 0)
+    cout << "0";
     return 0;
 }
